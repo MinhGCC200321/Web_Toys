76f4b260becfabea7550b6468e227dc2f0fb952f
TruongQuangMin <minhtqgcc200321@fpt.edu.vn>
2022-10-20 16:30:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include_once("connect.php");
         <div class="container">
             <div class="navbar">
                 <div class="logo">
-                    <a href="index.php"><img src="./images/logo.png" width="250px"></a>
+                    <a href="index.php"><img src="./images/logo.png" width="180px"></a>
                 </div>
                 <nav>
                     <ul id="MenuItems"> 
