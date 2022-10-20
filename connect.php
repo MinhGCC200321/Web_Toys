7fcbd4ff6f0b50299ff53c43bd86be624c3313e7
TruongQuangMin <minhtqgcc200321@fpt.edu.vn>
2022-10-20 08:43:17 +0700

    git add .

--- connect.php
+++ connect.php
@@ -10,7 +10,7 @@ $db = "asm";
 //     die("Failed ".$conn->connect_error);
 // }
 
-$Connect = pg_connect("");
+$Connect = pg_connect("postgres://mkxhtghzptlitn:15f9b316d2e2d903325cb65307a41302a444787ff46e3c89f8434f1f4b6cbe56@ec2-54-157-79-121.compute-1.amazonaws.com:5432/dbb867pvvv3miu");
 	if(!$Connect){
 		die("Connection failed");
 	}
