1678b299c9d9aec07c2143bc2273b83d8537d619
TruongQuangMin <minhtqgcc200321@fpt.edu.vn>
2022-10-22 07:52:28 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,19 @@
 <?php
 include_once("connect.php");
 
+// if(isset($_GET['id'])){
+//     $delQuery = "Delete from cart where p_id='".$_GET['id']."'";
+//     if (pg_query($conn, $delQuery)) {
+//         echo " <script>
+//         alert('Delete successful');
+//         window.location= 'manager.php?id=".$_GET['id']."'
+//         </script>";
+//     }else {
+//         echo "Error: "; // in loi tron pg
+//     }
+// }
 if(isset($_GET['id'])){
-    $delQuery = "Delete from cart where p_id='".$_GET['id']."'";
-    if (pg_query($conn, $delQuery)) {
-        echo " <script>
-        alert('Delete successful');
-        window.location= 'manager.php?id=".$_GET['id']."'
-        </script>";
-    }else {
-        echo "Error: "; // in loi tron pg
-    }
-}
-if(isset($_GET['id'])){
-    $delQuery = "Delete from product where Product_ID='".$_GET['id']."'";
+    $delQuery = "Delete from product where p_id='".$_GET['id']."'";
     if(pg_query($conn, $delQuery)) {
         echo " <script>  
         window.location = 'manager.php?status=delete';
