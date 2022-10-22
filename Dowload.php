66d1bf074b3057da483549e1382a0110054c964b
TruongQuangMin <minhtqgcc200321@fpt.edu.vn>
2022-10-22 21:30:32 +0700

    git add .

--- Dowload.php
+++ Dowload.php
@@ -12,27 +12,6 @@
 </style>
 </head>
 <body>
-<?php
-if(isset($_REQUEST["file"])){
-    // Get parameters
-    $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
-    $filepath = "images/" . $file;
-    
-    // Process download
-    if(file_exists($filepath)) {
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($filepath));
-        flush(); // Flush system output buffer
-        readfile($filepath);
-        exit;
-    }
-}
-?>
     <?php
     // Array containing sample image file names
     $images = array("kites.jpg", "balloons.jpg");
