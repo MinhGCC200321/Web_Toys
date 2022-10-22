ee7c75c01cd002884249836e87e74bb7b0d1273d
TruongQuangMin <minhtqgcc200321@fpt.edu.vn>
2022-10-22 21:53:35 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -52,13 +52,14 @@
                         
                         // Process download
                         if(file_exists($filepath)) {
-                            header('Content-Description: File Transfer');
-                            header('Content-Type: application/octet-stream');
-                            header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
-                            header('Expires: 0');
-                            header('Cache-Control: must-revalidate');
-                            header('Pragma: public');
-                            header('Content-Length: ' . filesize($filepath));
+                            // header('Content-Description: File Transfer');
+                            // //header('Content-Type: application/octet-stream');
+                            // header('Content-Type: application/octet-stream');
+                            // header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+                            // header('Expires: 0');
+                            // header('Cache-Control: must-revalidate');
+                            // header('Pragma: public');
+                            // header('Content-Length: ' . filesize($filepath));
                             flush(); // Flush system output buffer
                             readfile($filepath);
                         }
