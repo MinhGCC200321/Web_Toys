d65e3dcaab02b3968166d653aa6ef6a16af6313a
TruongQuangMin <minhtqgcc200321@fpt.edu.vn>
2022-10-25 09:30:59 +0700

    Update login.php

--- login.php
+++ login.php
@@ -5,12 +5,12 @@ session_start();
 if(isset($_POST['btnLogin'])){
     $uname = pg_escape_string($conn, trim($_POST['Username']));
     $pwd = md5(trim($_POST['Password']));
-    echo "<script>  
-        alert('Wrong username or password: $uname - $pwd);
-    </script>";
     $sql = "SELECT * FROM userta WHERE username = '$uname' and password = '$pwd'";
     $re = pg_query($conn,$sql);
     if(pg_num_rows($re) > 0 ){
+        echo "<script>  
+        alert('Wrong username or password: $uname - $pwd);
+    </script>";
         // $_SESSION['user'] = $uname;
         // $_SESSION['timeout'] = time();
         header("Location: index.php");
